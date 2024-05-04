04da1d1a0b18b54cb06ef69b0626d9a562e9296b
David <161784199+pellepaltnackepro957@users.noreply.github.com>
2024-05-04 18:10:50 +0200

    Update NebulaHubClear.lua

--- NebulaHubClear.lua
+++ NebulaHubClear.lua
@@ -22,10 +22,10 @@ local Window = Rayfield:CreateWindow({
        Title = "Nebula Hub Key",
        Subtitle = "Key System",
        Note = "Join our discord server, and go to the key channel to get the key.",
-       FileName = "nebulahublatestkey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"NebulaHubOnTop943"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       FileName = "nebulahublatestkey",
+       SaveKey = false,
+       GrabKeyFromSite = false,
+       Key = {"NebulaHubOnTop943"}
     }
  })
  -- End of window --
